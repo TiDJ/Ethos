455e7f212921c727d6c8c2b9c5022a5c3eff7562
Tom Jamon <contact@tomjamon.com>
2019-09-10 11:28:03 +0200

    Update background for login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,17 +2,14 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center backcolor">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
+                <h1>SE CONNECTER</h1>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -26,7 +23,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -45,7 +42,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Se rappeler de moi') }}
                                     </label>
                                 </div>
                             </div>
@@ -54,19 +51,18 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Connection') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Mot de passe oublié ?') }}
                                     </a>
                                 @endif
                             </div>
                         </div>
                     </form>
                 </div>
-            </div>
         </div>
     </div>
 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,183 +2,181 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center backcolor">
         <div class="col-md-12">
-            <div class="card colorregister">
-                <div class="card-header text-center modregister ">S'inscrire</div>
+            <h1>S'INSCRIRE</h1>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+            <div class="card-body">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-                        <!-- Nom -->
-                        <div class="form-group row">
-                            <label for="name" class="col-md-2 col-form-label text-md-right"></label>
+                    <!-- Nom -->
+                    <div class="form-group row">
+                        <label for="name" class="col-md-2 col-form-label text-md-right"></label>
 
-                            <div class="col-md-8">
-                                <input id="name" placeholder="Nom de compte*" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <div class="col-md-8">
+                            <input id="name" placeholder="Nom de compte*" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="email" class="col-md-2 col-form-label text-md-right"></label>
+
+                        <div class="col-md-8">
+                            <input id="email" type="email" placeholder="Adresse Email*" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password" class="col-md-2 col-form-label text-md-right">
+
+                        </label>
+
+                        <div class="col-md-4">
+                            <input id="password" placeholder="Mot de passe*" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-2 col-form-label text-md-right"></label>
 
-                            <div class="col-md-8">
-                                <input id="email" type="email" placeholder="Adresse Email*" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
+
+                        <div class="col-md-4">
+                            <input id="password-confirm" placeholder="Confirmation de mot de passe*" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-2 col-form-label text-md-right">
+                    <div class="form-group row">
+                        <label for="bio" class="col-md-2 col-form-label text-md-right">
 
-                            </label>
-
-                            <div class="col-md-4">
-                                <input id="password" placeholder="Mot de passe*" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-
-
-
-
-                            <div class="col-md-4">
-                                <input id="password-confirm" placeholder="Confirmation de mot de passe*" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        </label>
+                        <div class="col-md-8">
+                            <textarea id="bio" placeholder="Biographie" class="form-control" name="bio"></textarea>
+                            @error('bio')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="bio" class="col-md-2 col-form-label text-md-right">
+                    <hr>
 
-                            </label>
-                            <div class="col-md-8">
-                                <textarea id="bio" placeholder="Biographie" class="form-control" name="bio"></textarea>
-                                @error('bio')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
+                    <div class="form-group row">
+                        <label for="main_name" class="col-md-2 col-form-label text-md-right"></label>
+
+                        <div class="col-md-8">
+                            <input id="main_name" placeholder="[Main] Nom du personnage principal*" type="text" class="form-control @error('main_name') is-invalid @enderror" name="main_name" value="{{ old('main_name') }}" required>
+
+                            @error('main_name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="main_class" class="col-md-2 col-form-label text-md-right"></label>
+                        <div class="col-md-4">
+                            <select class="form-control @error('main_class') is-invalid @enderror" name="main_class">
+                                <option value="Mage">Mage</option>
+                                <option value="Guerrier">Guerrier</option>
+                                <option value="Chasseur">Chasseur</option>
+                                <option value="Demoniste">Démoniste</option>
+                                <option value="Druide">Druide</option>
+                                <option value="Paladin">Paladin</option>
+                                <option value="Pretre">Prêtre</option>
+                                <option value="Voleur">Voleur</option>
 
-                        <hr>
-
-                        <div class="form-group row">
-                            <label for="main_name" class="col-md-2 col-form-label text-md-right"></label>
-
-                            <div class="col-md-8">
-                                <input id="main_name" placeholder="[Main] Nom du personnage principal*" type="text" class="form-control @error('main_name') is-invalid @enderror" name="main_name" value="{{ old('main_name') }}" required>
-
-                                @error('main_name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
+                            </select>
+                            @error('main_class')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="form-group row">
-                            <label for="main_class" class="col-md-2 col-form-label text-md-right"></label>
-                            <div class="col-md-4">
-                                <select class="form-control @error('main_class') is-invalid @enderror" name="main_class">
-                                    <option value="Mage">Mage</option>
-                                    <option value="Guerrier">Guerrier</option>
-                                    <option value="Chasseur">Chasseur</option>
-                                    <option value="Demoniste">Démoniste</option>
-                                    <option value="Druide">Druide</option>
-                                    <option value="Paladin">Paladin</option>
-                                    <option value="Pretre">Prêtre</option>
-                                    <option value="Voleur">Voleur</option>
+                        <div class="col-md-4">
+                            <select class="form-control @error('main_role') is-invalid @enderror" name="main_role">
+                                <option value="Heal">Heal</option>
+                                <option value="Dps">Dps</option>
+                                <option value="Tank">Tank</option>
+                            </select>
+                            @error('main_role')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="reroll_name" class="col-md-2 col-form-label text-md-right"></label>
 
-                                </select>
-                                @error('main_class')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                            <div class="col-md-4">
-                                <select class="form-control @error('main_role') is-invalid @enderror" name="main_role">
-                                    <option value="Heal">Heal</option>
-                                    <option value="Dps">Dps</option>
-                                    <option value="Tank">Tank</option>
-                                </select>
-                                @error('main_role')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="reroll_name" class="col-md-2 col-form-label text-md-right"></label>
+                        <div class="col-md-8">
+                            <input type="text" placeholder="[Reroll] Nom du personnage secondaire" class="form-control @error('reroll_name') is-invalid @enderror" name="reroll_name" value="{{ old('reroll_name') }}">
 
-                            <div class="col-md-8">
-                                <input type="text" placeholder="[Reroll] Nom du personnage secondaire" class="form-control @error('reroll_name') is-invalid @enderror" name="reroll_name" value="{{ old('reroll_name') }}">
-
-                                @error('reroll_name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
+                            @error('reroll_name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="form-group row">
-                            <label for="reroll_class" class="col-md-2 col-form-label text-md-right"></label>
-                            <div class="col-md-4">
-                                <select class="form-control @error('reroll_class') is-invalid @enderror" name="reroll_class" id="reroll_class">
-                                    <option value="Mage">Mage</option>
-                                    <option value="Guerrier">Guerrier</option>
-                                    <option value="Chasseur">Chasseur</option>
-                                    <option value="Demoniste">Démoniste</option>
-                                    <option value="Druide">Druide</option>
-                                    <option value="Paladin">Paladin</option>
-                                    <option value="Pretre">Prêtre</option>
-                                    <option value="Voleur">Voleur</option>
-                                </select>
-                                @error('reroll_class')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                            <div class="col-md-4">
-                                <select class="form-control @error('reroll_role') is-invalid @enderror" name="reroll_role" id="reroll_role">
-                                    <option value="Heal">Heal</option>
-                                    <option value="Dps">Dps</option>
-                                    <option value="Tank">Tank</option>
-                                </select>
-                                @error('reroll_role')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="reroll_class" class="col-md-2 col-form-label text-md-right"></label>
+                        <div class="col-md-4">
+                            <select class="form-control @error('reroll_class') is-invalid @enderror" name="reroll_class" id="reroll_class">
+                                <option value="Mage">Mage</option>
+                                <option value="Guerrier">Guerrier</option>
+                                <option value="Chasseur">Chasseur</option>
+                                <option value="Demoniste">Démoniste</option>
+                                <option value="Druide">Druide</option>
+                                <option value="Paladin">Paladin</option>
+                                <option value="Pretre">Prêtre</option>
+                                <option value="Voleur">Voleur</option>
+                            </select>
+                            @error('reroll_class')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-12 offset-md-5">
-                                <button type="submit" class="btn btn-primary">
-                                    S'enregistrer
-                                </button>
-                            </div>
+                        <div class="col-md-4">
+                            <select class="form-control @error('reroll_role') is-invalid @enderror" name="reroll_role" id="reroll_role">
+                                <option value="Heal">Heal</option>
+                                <option value="Dps">Dps</option>
+                                <option value="Tank">Tank</option>
+                            </select>
+                            @error('reroll_role')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-12 offset-md-5">
+                            <button type="submit" class="btn btn-primary">
+                                S'enregistrer
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
